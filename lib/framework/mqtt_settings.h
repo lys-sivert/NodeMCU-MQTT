7a67a27cf9f06d4ef194ea8e5a2a5619b20b3d00
sivert-lysnes <vuvulala14@gmail.com>
2023-10-26 15:13:04 +0200

    Added some documentation

--- lib/framework/mqtt_settings.h
+++ lib/framework/mqtt_settings.h
@@ -16,6 +16,24 @@ void init(const char *broker, const char *name);
 void init(const char *broker, const char *name, int port);
 void update();
 
+/**
+ * @brief Adds a command to the MQTT client, a command is simply a function that is called when a message is recieved on
+ * the specifiet topic. The topic is automatically subscribed to
+ *
+ * @param path the MQTT topic to listen to
+ * @param callback a function that takes in the MQTT payload as input
+ * @return true if the command is succesfully added
+ */
 bool add_command(char *path, MQTT_CALLBACK callback);
+
+/**
+ * @brief Adds a datapoint to the MQTT client, a datapoint is simply a function that is called on an interval.
+ * The callback is executed every [interval]ms, if the mode is set to change, the value is only published on a changed
+ * value, or else the value is published every interval
+ *
+ * @param path the MQTT topic to write to
+ * @param callback a function that returns a String containing the data
+ * @return true if the datapoint is succesfully added
+ */
 bool add_datapoint(char *path, SendMode mode, uint32_t interval, MQTT_DATA_CALLBACK callback);
 }  // namespace MQTT
